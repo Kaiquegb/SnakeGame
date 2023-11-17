395b877e5e1224fe61a4c0ae72c90a1ee83e997c
Kiquegb <kaiqueghsilva@gmail.com>
2023-11-17 12:27:07 -0300

    structs e funções void

--- src/main.c
+++ src/main.c
@@ -5,14 +5,28 @@
 #include "keyboard.h"
 #include "timer.h"
 
-Cobrinha cobra;
+// struct para representar a cobra
+typedef struct {
+  int x, y;
+} Ponto;
+
+struct Snake {
+  Ponto cabeca;
+};
+
+// struct global para representar a cobra
+struct Snake cobra;
 
 int incX = 1, incY = 1;
 int x = 3, y = 2;
 
+// declaração da função drawSnake
+void drawSnake();
+void screenPutChar(char ch){
+     printf("%c", ch);
+}
 
-void NomeJogo(int nextX, int nextY)
-{
+void NomeJogo(int nextX, int nextY) {
     screenGotoxy(x, y);
     x = nextX;
     y = nextY;
@@ -20,15 +34,12 @@ void NomeJogo(int nextX, int nextY)
     printf("SNAKE GAME");
 }
 
-void drawSnake() {
-    // desenha a cobra na tela
-    screenGotoxy(cobra.cabeca.x, cobra.cabeca.y);
-    screenPutChar('*');
-}
-
-int main()
-{
+int main() {
     static int ch = 0;
+
+    // inicializa a cobra
+    cobra.cabeca.x = 0;
+    cobra.cabeca.y = 0;
 
     screenInit(1);
     keyboardInit();
@@ -36,15 +47,26 @@ int main()
     NomeJogo(x, y);
     screenUpdate();
 
-    while (ch != 10) //enter
+    while (ch != 10) // Enter
     {
         // Handle user input
-        if (keyhit())
-        {
+        if (keyhit()) {
             ch = readch();
             screenUpdate();
         }
 
+        // Atualiza a posição da cobra
+        if (ch == 'W') {
+            cobra.cabeca.y -= incY;
+        } else if (ch == 'S') {
+            cobra.cabeca.y += incY;
+        } else if (ch == 'A') {
+            cobra.cabeca.x -= incX;
+        } else if (ch == 'D') {
+            cobra.cabeca.x += incX;
+        }
+
+        drawSnake();
     }
 
     keyboardDestroy();
@@ -52,4 +74,11 @@ int main()
     timerDestroy();
 
     return 0;
+}
+
+// implementação da função drawSnake
+void drawSnake() {
+    // desenha a cobra na tela usando a biblioteca
+    screenGotoxy(cobra.cabeca.x, cobra.cabeca.y);
+    screenPutChar('*');
 }
